87cee2704832c9155e8fb015ab34aa9a44aac8b9
kedzkiest <kaede12kazu22@gmail.com>
2022-11-19 12:04:11 +0900

    can blend two images, manipulate whether to blend  at runtime

---
+++ LearningShader_Cg/Assets/Script/6_Passes&Blending/DecalOnOff.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DecalOnOff : MonoBehaviour
+{
+    Material mat;
+    bool showDecal = false;
+
+    void OnMouseDown(){
+        showDecal = !showDecal;
+        if(showDecal){
+            mat.SetFloat("_ShowDecal", 1);
+        }
+        else{
+            mat.SetFloat("_ShowDecal", 0);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().sharedMaterial;
+    }
+}
